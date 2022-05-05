e355c40ffa4002c9aba00ec2c70aeae69a20bfe5
MyagkiyBogdan <myagkiybogdan@gmail.com>
2022-05-05 03:31:56 +0300

    finisted script

--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -4,19 +4,18 @@ const KEY = '27181891-3778ead93a3f3d83793e3b927';
 export class FetchDataPhotos {
   constructor() {}
 
-  fetchPhotos(query) {
-    const url = `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .then(photos => {
+  async fetchPhotos(query, page) {
+    const url = `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(response.status);
+      } else {
+        const photos = await response.json();
         return photos;
-      })
-      .catch(error => console.log(`This is class catched error: ${error}`));
+      }
+    } catch (error) {
+      console.log(`This is class catched error: ${error}`);
+    }
   }
 }
